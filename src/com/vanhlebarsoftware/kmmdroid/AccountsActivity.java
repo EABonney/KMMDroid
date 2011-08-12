20c83679c5173aba14bbd32dd1d50e242decb87c
Eric Bonney <eric@vanhlebarsoftware.com>
2011-08-11 23:18:10 -0400

    Begin adding AccountsActivity

---
+++ src/com/vanhlebarsoftware/kmmdroid/AccountsActivity.java
@@ -0,0 +1,9 @@
+package com.vanhlebarsoftware.kmmdroid;
+
+import android.app.Activity;
+
+public class AccountsActivity extends Activity
+{
+	private static final String TAG = "AccountsActivity";
+	
+}
