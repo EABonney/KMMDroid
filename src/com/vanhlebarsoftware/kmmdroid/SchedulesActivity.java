ce016090b988df2d867f656e3a3093e84f2f886f
Eric Bonney <eric@vanhlebarsoftware.com>
2012-01-23 14:59:17 -0500

    Finished initial listing of Schedules, need to add in CRUD functions for schedules and maybe  we have an issue with the SQL statement when a schedule has an endDate and lastPayment is after it.

--- src/com/vanhlebarsoftware/kmmdroid/SchedulesActivity.java
+++ src/com/vanhlebarsoftware/kmmdroid/SchedulesActivity.java
@@ -16,7 +16,8 @@ public class SchedulesActivity extends Activity
 	private static final String dbTable = "kmmSchedules, kmmSplits, kmmPayees";
 	private static final String[] dbColumns = { "kmmSchedules.id AS _id", "kmmSchedules.name AS Description", "occurenceString", "nextPaymentDue", 
 												"endDate", "lastPayment", "valueFormatted", "kmmPayees.name AS Payee" };
-	private static final String strSelection = "kmmSchedules.id = kmmSplits.transactionId AND kmmSplits.payeeId = kmmPayees.id AND nextPayment > 0";
+	private static final String strSelection = "kmmSchedules.id = kmmSplits.transactionId AND kmmSplits.payeeId = kmmPayees.id AND nextPaymentDue > 0" + 
+												" AND ((occurenceString = 'Once' AND lastPayment IS NULL) OR occurenceString != 'Once')";
 	private static final String strOrderBy = "nextPaymentDue ASC";
 	static final String[] FROM = { "Description", "occurenceString", "nextPaymentDue", "valueFormatted", "Payee" };
 	static final int[] TO = { R.id.srDescription, R.id.srFrequency, R.id.srNextDueDate, R.id.srAmount, R.id.srPayee };
